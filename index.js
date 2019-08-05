08a0cf018b8e591aa0073bd40396d3dd701fad9c
ragnar <ragnar@templatemonster.me>
2019-08-05 18:38:41 +0300

    FIX

--- index.js
+++ index.js
@@ -5,3 +5,4 @@ app.use('/', express.static(__dirname + '/docs'));
 
 app.set('port', (process.env.PORT || 5000));
 app.listen(app.get('port'));
+console.log( 'Server success started localhost:5000' )
