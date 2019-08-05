0e68f6b06386db84fc01de4c7726e363104efdff
ragnar <ragnar@templatemonster.me>
2019-08-05 19:03:42 +0300

    ADD: index.js

--- docs/script.js
+++ docs/script.js
@@ -1,1 +1,21 @@
-console.log( 'test' );
+"use strict";
+(function () {
+	let
+		list = [],
+		input = document.querySelector( '#input' ),
+		button = document.querySelector( '#addButton' );
+
+	button.addEventListener( 'click', function() {
+
+		let value = input.value;
+
+		if( value.length < 1 ) {
+			return ;
+		}
+
+		list.push( value );
+		input.value = '';
+		console.log( list );
+	} );
+
+}());
