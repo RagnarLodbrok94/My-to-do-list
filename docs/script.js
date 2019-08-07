5733722ffe3b10925f0e22dee0d892260f0f913d
ragnar <ragnar@templatemonster.me>
2019-08-07 19:08:05 +0300

    ADD: ADD rendering list && removing items for List && localStorage

--- docs/script.js
+++ docs/script.js
@@ -1,21 +1,62 @@
 "use strict";
 (function () {
 	let
-		list = [],
+		array = [ 'Mosby', 'Ragnar', 'Atom' ],
+		list = document.getElementById( 'list' ),
 		input = document.querySelector( '#input' ),
 		button = document.querySelector( '#addButton' );
 
-	button.addEventListener( 'click', function() {
-
+	function addItem() {
 		let value = input.value;
 
 		if( value.length < 1 ) {
 			return ;
 		}
 
-		list.push( value );
+		array.push( value );
 		input.value = '';
-		console.log( list );
-	} );
+		console.log( array );
 
+		addItemInDom( value, list.length - 1 );
+
+		renderList();
+		saveToLocalStorage();
+	}
+
+	function removeItem( index ) {
+		delete array[ index ];
+
+		renderList();
+		saveToLocalStorage();
+	}
+
+	function addItemInDom( item, index ) {
+		let listItem = document.createElement( 'li' ),
+			removeButton = document.createElement( 'button' );
+		listItem.setAttribute( "data-index", index );
+		listItem.innerHTML = item;
+		list.appendChild( listItem );
+
+		removeButton.innerHTML = 'X';
+		removeButton.addEventListener( 'click', function() {
+			removeItem( index );
+		} );
+
+		list.appendChild( removeButton );
+	}
+
+	function renderList() {
+		list.innerHTML = '';
+		array.forEach( ( element, index ) => {
+			addItemInDom( element, index );
+		} );
+	}
+
+	function saveToLocalStorage() {
+		localStorage.setItem( 'ragnar_Items', JSON.stringify( list ) );
+	}
+
+	button.addEventListener( 'click', addItem );
+
+	renderList();
 }());
